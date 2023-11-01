bd77c1340684ac683c094d151df14a43076f1630
AnitaOwen <anitaowen@pursuit.org>
2023-11-01 14:55:34 -0400

    created generateCookie file

---
+++ scripts/generateCookie.js
@@ -0,0 +1,12 @@
+function generateCookie(name, price, inStock, containsNuts, isVegan, imageLink, notes) {
+
+
+
+}
+
+function createListItem(name, price, inStock, containsNuts, isVegan, imageLink, notes){
+    const li = document.createElement("li");
+    li.classList.add("single-cookie");
+}
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,16 @@
 const form = document.querySelector('form');
 form.addEventListener('submit', (event)=>{
     event.preventDefault()
+    // console.log(event)
+    const {
+        name,
+        price,
+        inStock,
+        containsNuts,
+        isVegan,
+        imageLink,
+        notes
+    } = event.target;
+
+    
 });
